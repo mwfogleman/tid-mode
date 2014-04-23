a3cb20e858950011d8e43d2670016569db79e097
Michael Fogleman <michaelwfogleman@gmail.com>
2014-04-23 12:46:02 +0530

    Whitespace changes.

--- tid-mode.el
+++ tid-mode.el
@@ -2,6 +2,7 @@
   (and
    (> (length (buffer-file-name)) 4)
    (string-equal (substring (buffer-file-name) -4) ".tid")))
+
 (defun tid-time ()
   "If called in a tiddler file, this function updates the metadata to
   reflect the modification time."
@@ -18,5 +19,7 @@
    "A major mode for editing TiddlyWiki5 (.tid) files."
    (add-hook 'tid-mode-hook 'turn-on-orgstruct)
    (add-hook 'tid-mode-hook 'subword-mode))
+
 (add-to-list 'auto-mode-alist '("\\.tid\\'" . tid-mode))
+
 (provide 'tid-mode)
